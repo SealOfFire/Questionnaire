313b859f90be0b685ecafadf23fd09dbd5f6d888
sealoffire <sealoffire@hotmail.com>
2016-07-28 14:48:32 +0800

    数据库连接测试

---
+++ src/questionnaire/dal/MySQLManager.java
@@ -0,0 +1,103 @@
+package questionnaire.dal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ParameterMetaData;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/** 数据库操作管理 */
+public class MySQLManager {
+
+	/** 构造函数 */
+	public MySQLManager() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 获取数据库连接
+	 * 
+	 * @throws SQLException
+	 */
+	public Connection getConnect() throws SQLException {
+		Connection connection = DriverManager.getConnection(
+				"jdbc:mysql://localhost/questionnairedb?user=root&password=root&characterEncoding=utf8&useSSL=false");
+		return connection;
+	}
+
+	/** 检索数据 */
+	public void select(String query) {
+		Object[] parameters = new Object[0];
+		this.select(query, parameters);
+	}
+
+	/** 检索数据 */
+	public void select(String query, Object[] parameters) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+
+			conn = this.getConnect();
+			pstmt = conn.prepareStatement(query);
+			ParameterMetaData pmd = pstmt.getParameterMetaData();
+			for (int i = 0; i < parameters.length; i++) {
+				pstmt.setObject(i + 1, parameters[i]);
+			}
+
+			// 执行查询
+			rs = pstmt.executeQuery();
+
+			// TODO 测试代码
+			while (rs.next()) {
+				System.out.println(rs.getString("QuestionnaireID"));
+			}
+
+			rs.close();
+			// 关闭连接
+			pstmt.close();
+			pstmt = null;
+			conn.close();
+			conn = null;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// 出错时关闭连接
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				rs = null;
+			}
+
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				pstmt = null;
+			}
+
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				conn = null;
+			}
+		}
+	}
+
+}
--- src/questionnaire/test/TestMySqlConnect.java
+++ src/questionnaire/test/TestMySqlConnect.java
@@ -4,8 +4,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 
 import org.junit.Test;
+
+import questionnaire.dal.MySQLManager;
 
 /** 测试数据库连接 */
 public class TestMySqlConnect {
@@ -14,26 +20,84 @@ public class TestMySqlConnect {
 	@Test
 	public void testConnect() {
 		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+
 			Connection conn = DriverManager
-					.getConnection("jdbc:mysql://localhost/test?" + "user=minty&password=greatsqldb");
+					.getConnection("jdbc:mysql://localhost/questionnairedb?user=root&password=root");
 
 			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT foo FROM bar");
+			ResultSet rs = stmt.executeQuery("select * from questionnaire");
 
-			if (stmt.execute("SELECT foo FROM bar")) {
+			if (stmt.execute("select * from questionnaire")) {
 				rs = stmt.getResultSet();
 			}
 
-			stmt.executeUpdate(
-					"INSERT INTO autoIncTutorial (dataField) " + "values ('Can I Get the Auto Increment Field?')",
+			int value = stmt.executeUpdate(
+					"insert into questionnaire (QuestionnaireID,Title,TimeLimit) " + "values (uuid(),'title',20)",
 					Statement.RETURN_GENERATED_KEYS);
+			System.out.println("value");
 
 			rs = stmt.getGeneratedKeys();
 
 			rs.close();
+			stmt.close();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	/** 连接池 */
+	@Test
+	public void testConnectPooling() {
+		Connection conn = null;
+		Statement stmt = null;
+		try {
+			InitialContext ctx = new InitialContext();
+			DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/MySQLDB");
+
+			conn = ds.getConnection();
+			stmt = conn.createStatement();
+			stmt.execute("select * from questionnaire");
+
+			stmt.close();
+			stmt = null;
+
+			conn.close();
+			conn = null;
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (Exception sqlex) {
+					// ignore, as we can't do anything about it here
+				}
+				stmt = null;
+			}
+
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (Exception sqlex) {
+					// ignore, as we can't do anything about it here
+				}
+				conn = null;
+			}
+		}
+	}
+
+	@Test
+	public void testSelect() {
+		MySQLManager manage = new MySQLManager();
+		ArrayList<Object> parameters = new ArrayList<Object>();
+		parameters.add("title");
+		parameters.add(20);
+		manage.select("select * from questionnaire where Title=? and TimeLimit=?", parameters.toArray());
+
+		manage.select("select * from questionnaire");
 	}
 }
